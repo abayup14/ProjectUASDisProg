6f5bbf207a2149cdfb8d999e3c2e93faed42dde0
andrebayup14 <abayup1498@gmail.com>
2023-07-06 06:14:04 +0700

    Update BMILoginFrame.java

--- BMICalculator/src/bmicalculator/BMILoginFrame.java
+++ BMICalculator/src/bmicalculator/BMILoginFrame.java
@@ -11,6 +11,7 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -33,7 +34,7 @@ public class BMILoginFrame extends javax.swing.JFrame implements Runnable{
     
     private void getMessage() {
         try {
-            jOptionPane1.showMessageDialog(this, this.input.readLine()+"\n");
+            JOptionPane.showMessageDialog(this, this.input.readLine()+"\n");
         } catch (IOException ex) {
             Logger.getLogger(BMILoginFrame.class.getName()).log(Level.SEVERE, null, ex);
         }
